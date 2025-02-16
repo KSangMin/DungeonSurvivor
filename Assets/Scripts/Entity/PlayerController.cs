df80f01a98f719bd1220b7e2c32cb18a5b8cc877
KSangMin <fhfpszmfnwm@naver.com>
2025-02-16 18:48:00 +0900

    [Feat] 플레이어 이동 구현

---
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : BaseController
+{
+    private Camera _camera;
+
+    protected override void Start()
+    {
+        base.Start();
+        _camera = Camera.main;
+    }
+
+    protected override void HandleAction()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+        movementDir = new Vector2(hor, ver).normalized;
+
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 worldPos = _camera.ScreenToWorldPoint(mousePos);
+        lookDir = (worldPos - (Vector2)transform.position);
+
+        if(lookDir.magnitude < 0.9f)
+        {
+            lookDir = Vector2.zero;
+        }
+        else
+        {
+            lookDir = lookDir.normalized;
+        }
+    }
+}
